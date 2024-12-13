80d2ab6cceb40c02c927a4072a340d6368f526e5
JosephVolosin <joseph.p.volosin@nasa.gov>
2024-12-13 13:44:10 -0500

    Add API documents for External Events

--- sidebars.js
+++ sidebars.js
@@ -31,6 +31,7 @@ const sidebars = {
         'api/examples/activity-presets',
         'api/examples/advanced-extensions',
         'api/examples/tags',
+        'api/examples/external-events',
       ],
     },
   ],
